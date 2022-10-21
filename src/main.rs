53847d16ad0ce9fd6fae6e2f9ebdca81abfc2a8f
phoebx <phoebx@pm.com>
2022-10-21 14:14:25 -0400

    added multiple basic flags with suggestions

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,27 @@ struct CliArgs {
 // these derive attributes are neccessary to pass an enum value as cli params
 #[derive(ValueEnum, Clone, Debug)]
 enum DesiredOutcomes {
+    Cli,
     Compress,
+    DocumentEditor,
+    Editor,
+    Encode,
     Encrypt,
+    Find,
+    Firewall,
+    Images,
+    List,
+    Manual,
+    // TODO: rename
+    NavigateLocal,
+    Networking,
+    PackageManager,
+    Partition,
+    Permission,
+    Print, // print to the console
+    RandomGenerator,
+    System,
+    VersionControl
 }
 
 // TODO:
@@ -30,27 +49,91 @@ fn main() -> Result<()> {
 // TODO:
 // extend this to have a json flag that will allow it to be piped into the next stage?
 // need to think this through further
-fn find_suggestons(desired_outcome: &DesiredOutcomes, mut writer: impl std::io::Write) -> () {
+fn find_suggestons(
+    desired_outcome: &DesiredOutcomes,
+    mut writer: impl std::io::Write,
+) -> Result<(), std::io::Error> {
+    let json_output = create_json_output();
+    // TODO:
+    // this returns a result that doesn't handle all errors, need to read the book a bit more
+    // to find out how to add error handling to match statements
+    let output = match desired_outcome {
+        // two different ways to acess the field values
+        DesiredOutcomes::Cli => &json_output.get("cli").unwrap(),
+        DesiredOutcomes::Compress => &json_output.get("compress").unwrap(),
+        // should this have subcommands? document_editor ide_editor?
+        DesiredOutcomes::Editor => &json_output["editor"],
+        DesiredOutcomes::DocumentEditor => &json_output["document_editor"],
+
+        DesiredOutcomes::Encode => &json_output["encode"],
+        DesiredOutcomes::Encrypt => &json_output["encrypt"],
+        DesiredOutcomes::Find => &json_output["find"],
+        DesiredOutcomes::Firewall => &json_output["firewall"],
+        DesiredOutcomes::Images => &json_output["images"],
+        // prints the entire nested json
+        // DesiredOutcomes::List => &json_output["list"],
+        // prints just the applications
+        DesiredOutcomes::List => &json_output["list"]["apps"],
+        DesiredOutcomes::Manual => &json_output["manual"],
+        DesiredOutcomes::NavigateLocal => &json_output["navigate_local"],
+        DesiredOutcomes::Networking => &json_output["networking"],
+        DesiredOutcomes::PackageManager => &json_output["package_manager"],
+        DesiredOutcomes::Partition => &json_output["partition"],
+        DesiredOutcomes::Permission => &json_output["permissions"],
+        DesiredOutcomes::Print => &json_output["print"],
+        DesiredOutcomes::RandomGenerator => &json_output["random_generator"],
+        DesiredOutcomes::System => &json_output["system"],
+        DesiredOutcomes::VersionControl => &json_output["version_control"],
+        // no need for having _ since it's an enum
+    };
+
+    writeln!(writer, "{}", output)
+}
+
+fn create_json_output() -> serde_json::Value {
     // TODO:
     // abstract this in such a way that it can be programatically added to.
     // I envision adding a suggestion is just running a function that updates the json structure
     // (but would this also allow updating the enum param possibilities?)
     let json_output: serde_json::Value = json!({
-        "compress": "gzip, zip, tar",
-        "encrypt": "gpg"
+        "cli": "clear, cp, scp, ",
+        "compress": "gzip, tar, zip",
+        "document_editor": "libreoffice, mdbook",
+        "editor": "gedit, nano, neo-vim, vim",
+        "encode": "base64",
+        "encrypt": "fscrypt, gpg",
+        "find": "find, grep, ripgrep",
+        "firewall": "ufw, firewall-cmd",
+        // should this be plural? and should this extend to ISOs?
+        "images": "imgp", // "pictures"
+        "images_iso": "docker, isoinfo", // "images"
+        // not sure if this is the correct way to go about this, could be a helper function in clap for --help on each
+        "list": {
+            "description":"list everything in a directory",
+            "apps":"dir, exa, ls, tree"
+        },
+        "manual": "cheat, man, tldr",
+        "navigate_local": "cd, pwd",
+        "networking": "arp, ifdown, ip, iw, ssh",
+        "package_manager": "apt, flatpack, pacman",
+        "passwords": "apg, hashcat",
+        "partition": "fdisk, gparted, lvm, lvresize", // should logical volumes be here?
+        "permissions": "chmod, chown, chroot",
+        "print": "bat, cat, chafa, head",
+        "random_generator": "apg",
+        "system": "arch, free, fstrim, fuser, jobs, kexec, lsb_release, lsusb, lsof",
+        // look up other vc standards,
+        "version_control": "git",
+
+        // cmp <- compare two files byte vs byte
+        // codespell <- find typos in a dir
+        // exif <- metadata on jpef files
+        // laptop-detect <- try to determine if on a laptop or desktop, whoa
+        "not_sure_but_useful": "cmp, codespell, exif, laptop-detect",
+
     });
-
-    // TODO:
-    // this returns a result that doesn't handle all errors, need to read the book a bit more
-    // to find out how to add error handling to match statements
-    match desired_outcome {
-        // two different ways to acess the field values
-        DesiredOutcomes::Compress => writeln!(writer, "{}", *json_output.get("compress").unwrap()),
-        DesiredOutcomes::Encrypt => writeln!(writer, "{}", json_output["encrypt"]),
-        // no need for having _ since it's an enum
-    };
+    return json_output;
 }
-
 #[test]
 fn find_a_suggestons() {
     // the writer
